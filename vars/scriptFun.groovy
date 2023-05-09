5d4207276dc92757e5373d9d91741cb85abae36f
varunsonavni <102613598+varunsonavni@users.noreply.github.com>
2023-05-09 20:13:54 +0530

    update

--- vars/scriptFun.groovy
+++ vars/scriptFun.groovy
@@ -1,5 +1,4 @@
 def call() {
   sh 'pwd'
-  sh 'ls -al'
-  sh 'echo Hi From DevOps Team'
+  sh 'echo Hello from first call'
 }
--- vars/scriptRepoName.groovy
+++ vars/scriptRepoName.groovy
@@ -1,5 +1,5 @@
-def call(String repo) {
+def call(String repo, String age) {
 
-  sh "echo Custom value is: ${repo}"
+  sh "echo Custom value is: ${repo}, age is ${age}"
   
 }
