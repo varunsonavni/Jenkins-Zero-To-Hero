f801eb10d0660cb2b148d17395fcec5138d73465
varunsonavni <102613598+varunsonavni@users.noreply.github.com>
2023-05-10 17:28:24 +0530

    update

--- vars/diffStage.groovy
+++ vars/diffStage.groovy
@@ -1,59 +1,66 @@
 def call(Map config = [:]) {
-
-pipeline {
-    
-    agent any 
-    
-    environment {
-        IMAGE_TAG = "${BUILD_NUMBER}"
-    }
-    
-    stages {
-
-
-        stage('Call from Share Library'){
-            steps{
-                scriptFun()
-            }
-        }
-
-        stage('Call from Share Library with custom value'){
-            steps{
-                scriptRepoName("varun@varun","20")
-            }
-        }
-
-        stage('Call from Share Library with any arguments 1-2-3 or many'){
-            steps{
-                customPara(name:"varun", surname:"sonavni")
-            }
-        }
-
-        // stage('Call from Share Library with any arguments 1-2-3 or manyff'){
-        //     steps{
-        //         diffStage()
-        //     }
-        // }
-        // diffStage(name:"varun", surname:"sonavni")
-        // diffStage()
-
-
-
-
-        stage('pipeline step'){
-            steps{
-                script{
-                    sh '''
-                    echo 'In jenkins file'
-                    
-                    '''
+    stage('Build') {
+                steps {
+                    sh 'echo Buidling...........###########'
                 }
             }
-        }
 
-        }
-    }
+            stage('Test') {
+                steps {
+                    sh 'echo "TESTING$$$$$$$$$$$$$$$$44"'
+                }
+            }
 
-
-
+            stage('Deploy') {
+                steps {
+                    sh 'echo "deploy%%%%%%%%%%%%%%%%"'
+                }
+            }
 }
+
+
+
+// def call(Map config = [:]) {
+//    pipeline {
+    
+//     agent any 
+    
+//     environment {
+//         IMAGE_TAG = "${BUILD_NUMBER}"
+//     }
+    
+//     stages {
+
+
+//         stage('Call from Share Library'){
+//             steps{
+//                 scriptFun()
+//             }
+//         }
+
+//         stage('Call from Share Library with custom value'){
+//             steps{
+//                 scriptRepoName("varun@varun","20")
+//             }
+//         }
+
+//         stage('Call from Share Library with any arguments 1-2-3 or many'){
+//             steps{
+//                 customPara(name:"varun", surname:"sonavni")
+//             }
+//         }
+
+//         stage('pipeline step'){
+//             steps{
+//                 script{
+//                     sh '''
+//                     echo 'In jenkins file'
+                    
+//                     '''
+//                 }
+//             }
+//         }
+
+//         }
+//     }
+// }
